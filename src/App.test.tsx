890c30258e92f4c565b1d4bf7ba938e344629e3d
Ian McCausland <ian.mccausland@tophat.com>
2024-02-20 10:39:11 -0500

    initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from './App'
+
+const customRender = (props = {}) => {
+  const user = userEvent.setup();
+  return {
+    user,
+    ...render(<App {...props} />),
+  }
+}
+
+it('renders the app', () => {
+  customRender();
+  expect(screen.getByRole('heading', { name: /hello world/i })).toBeInTheDocument();
+});
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <main>
+      <h1>Hello world</h1>
+    </main>
+  )
+}
+
+export default App
